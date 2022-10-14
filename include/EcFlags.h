dc14bb48bb3df6b6b8fd2c785f7742a163d7cb86
Yang.LUO <992534544@qq.com>
2022-10-14 17:00:15 +0800

    Add EcFlags
    Using gflags to process commandline flags

---
+++ include/EcFlags.h
@@ -0,0 +1,30 @@
+#ifndef EC_FLAGS_H
+#define EC_FLAGS_H
+
+#include <gflags/gflags.h>
+
+DECLARE_string(config);
+
+DECLARE_string(eni);
+
+DECLARE_int32(t);
+
+DECLARE_int32(b);
+
+DECLARE_int32(v);
+
+DECLARE_int32(a);
+
+DECLARE_bool(perf);
+
+DECLARE_int32(auxclk);
+
+DECLARE_int32(sp);
+
+DECLARE_string(log);
+
+DECLARE_string(i8254x);
+
+
+
+#endif // EC_FLAGS_H
